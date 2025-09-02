f05bee3c02ef0c21e7e35e508cf0bc2dd5438291
braj-cpu <brajkishorsah74@gmail.com>
2025-09-02 16:44:52 +0530

    Add User and Video Model

---
+++ models/user.model.js
@@ -0,0 +1,89 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true//makes field searcheable
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    fullname:{
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar:{
+        type: String,
+        required: true
+    },
+    coverImage:{
+        type: String,
+    },
+    watchHistory:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password:{
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken:{
+        type: String
+    }    
+} , {timestamps: true})
+
+userSchema.pre("save", async function (next){ // aage pass kardo
+    if(!this.isModified("password")) return next();//this refers to the document being saved
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+//Hooks are functions that run before or after certain Mongoose model actions.
+// Express middleware → works on HTTP request/response flow. Mongoose hooks → work on the database lifecycle (querying, saving, deleting docs).
+
+userSchema.methods.isPasswordCorrect= async function(password){
+    return await bcrypt.compare(password, this.password)//returns true/false
+}
+
+userSchema.methods.generateAccessToken= function(){
+    return jwt.sign(
+        {
+            _id: this.id,
+            email: this. email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken= function(){
+    return jwt.sign(
+        {
+            _id: this.id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+export const User = mongoose.model("User", userSchema)
